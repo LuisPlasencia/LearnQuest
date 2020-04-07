2f65898546d3f3037d832f9e7c480cb72ce2a544
Thunderscythe <cyserser@gmail.com>
2020-04-07 14:08:59 +0100

    T0.3.3 Modificación de pequeños defectos

--- app/src/main/java/es/ulpgc/eite/da/learnquest/QuizUnit.java
+++ app/src/main/java/es/ulpgc/eite/da/learnquest/QuizUnit.java
@@ -9,6 +9,6 @@ public class QuizUnit extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quiz_unit);
+        setContentView(R.layout.activity_perfil);
     }
 }
