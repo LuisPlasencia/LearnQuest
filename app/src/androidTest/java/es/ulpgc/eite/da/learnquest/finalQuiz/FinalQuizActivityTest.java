edd3598d4b894d2a201da007fbc01db7d43fa173
Luis Plasencia <luisplasenciapulido@gmail.com>
2020-04-09 00:37:33 +0100

    T3.8.2 Test Final Quiz, mejora de los test y corrección de errores

--- app/src/androidTest/java/es/ulpgc/eite/da/learnquest/finalQuiz/FinalQuizActivityTest.java
+++ app/src/androidTest/java/es/ulpgc/eite/da/learnquest/finalQuiz/FinalQuizActivityTest.java
@@ -48,6 +48,7 @@ public class FinalQuizActivityTest {
     String exp_needed = context.getResources().getString(R.string.exp_to_display);
     String t1_topic = context.getResources().getString(R.string.T1_Topic);
     String t2_topic = context.getResources().getString(R.string.T2_Topic);
+    String level = context.getResources().getString(R.string.level_display_final_quiz);
 
 
     @Test
@@ -61,6 +62,9 @@ public class FinalQuizActivityTest {
 
         ViewInteraction textView2 = onView(withId(R.id.exp_to_nextlevel));
         textView2.check(matches(withText(exp_needed)));
+
+        ViewInteraction textView3 = onView(withId(R.id.level_display));
+        textView3.check(matches(withText(level)));
 
         ViewInteraction progressBar = onView(withId(R.id.progressBar2));
         progressBar.check(matches(isDisplayed()));
@@ -99,8 +103,14 @@ public class FinalQuizActivityTest {
         ViewInteraction textView2 = onView(withId(R.id.exp_to_nextlevel));
         textView2.check(matches(withText(exp_needed)));
 
+        ViewInteraction textView3 = onView(withId(R.id.level_display));
+        textView3.check(matches(withText(level)));
+
         ViewInteraction progressBar = onView(withId(R.id.progressBar2));
         progressBar.check(matches(isDisplayed()));
+
+        ViewInteraction imageButton = onView(withContentDescription("Navigate up"));
+        imageButton.check(matches(isDisplayed()));
 
         //WHEN
         pressBack();
@@ -112,17 +122,14 @@ public class FinalQuizActivityTest {
         ViewInteraction viewGroup2 = onView(withId(R.id.linearLayout));
         viewGroup2.check(matches(isDisplayed()));
 
-        ViewInteraction textView3 = onView(withId(R.id.linearLayout));
-        textView3.check(matches(withText(t1_topic)));
+        ViewInteraction textView5 = onView(withId(R.id.t2_topic));
+        textView5.check(matches(withText(t2_topic)));
 
-        ViewInteraction textView4 = onView(withId(R.id.t2_topic));
-        textView4.check(matches(withText(t2_topic)));
-
-        ViewInteraction textView5 = onView(withId(R.id.t1_topic));
-        textView3.check(matches(isDisplayed()));
-
-        ViewInteraction textView6 = onView(withId(R.id.t2_topic));
+        ViewInteraction textView6 = onView(withId(R.id.t1_topic));
         textView6.check(matches(isDisplayed()));
+
+        ViewInteraction textView7 = onView(withId(R.id.t2_topic));
+        textView7.check(matches(isDisplayed()));
 
         ViewInteraction button = onView(withId(R.id.t1_topic_solve));
         button.check(matches(isDisplayed()));
@@ -147,8 +154,15 @@ public class FinalQuizActivityTest {
         ViewInteraction textView2 = onView(withId(R.id.exp_to_nextlevel));
         textView2.check(matches(withText(exp_needed)));
 
+        ViewInteraction textView3 = onView(withId(R.id.level_display));
+        textView3.check(matches(withText(level)));
+
         ViewInteraction progressBar = onView(withId(R.id.progressBar2));
         progressBar.check(matches(isDisplayed()));
+
+        ViewInteraction imageButton = onView(withContentDescription("Navigate up"));
+        imageButton.check(matches(isDisplayed()));
+
 
         //WHEN
         ViewInteraction appCompatImageButton = onView(withContentDescription("Navigate up"));
@@ -161,17 +175,17 @@ public class FinalQuizActivityTest {
         ViewInteraction viewGroup2 = onView(withId(R.id.linearLayout));
         viewGroup2.check(matches(isDisplayed()));
 
-        ViewInteraction textView3 = onView(withId(R.id.linearLayout));
-        textView3.check(matches(withText(t1_topic)));
-
-        ViewInteraction textView4 = onView(withId(R.id.t2_topic));
-        textView4.check(matches(withText(t2_topic)));
-
-        ViewInteraction textView5 = onView(withId(R.id.t1_topic));
-        textView3.check(matches(isDisplayed()));
+        ViewInteraction textView5 = onView(withId(R.id.linearLayout));
+        textView5.check(matches(withText(t1_topic)));
 
         ViewInteraction textView6 = onView(withId(R.id.t2_topic));
-        textView6.check(matches(isDisplayed()));
+        textView6.check(matches(withText(t2_topic)));
+
+        ViewInteraction textView7 = onView(withId(R.id.t1_topic));
+        textView7.check(matches(isDisplayed()));
+
+        ViewInteraction textView8 = onView(withId(R.id.t2_topic));
+        textView8.check(matches(isDisplayed()));
 
         ViewInteraction button = onView(withId(R.id.t1_topic_solve));
         button.check(matches(isDisplayed()));
