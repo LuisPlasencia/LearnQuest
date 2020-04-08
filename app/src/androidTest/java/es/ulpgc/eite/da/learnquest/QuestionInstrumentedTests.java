300d77dfbf8353cc1825aa1e5d0dcade1be03def
Thunderscythe <cyserser@gmail.com>
2020-04-08 20:50:08 +0100

    T0.4.3 Arreglando y ajustando algunos problemas con los ID's

--- app/src/androidTest/java/es/ulpgc/eite/da/learnquest/QuestionInstrumentedTests.java
+++ app/src/androidTest/java/es/ulpgc/eite/da/learnquest/QuestionInstrumentedTests.java
@@ -1,12 +1,8 @@
 package es.ulpgc.eite.da.learnquest;
 
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
 
-import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -19,10 +15,8 @@ import org.junit.runner.RunWith;
 import es.ulpgc.eite.da.learnquest.question.QuestionActivity;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.not;
@@ -31,5 +25,32 @@ import static org.hamcrest.Matchers.not;
 @RunWith(AndroidJUnit4.class)
 public class QuestionInstrumentedTests {
 
+    @Rule
+    public ActivityTestRule<QuestionActivity> activityTestRule =
+            new ActivityTestRule(QuestionActivity.class );
 
+
+    Context context =
+            InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+    String english_button = "English";
+    String maths_button = "Maths";
+    String geography_button = "Geography";
+
+    String math_level = context.getResources().getString(R.string.math_level);
+    String english_level = context.getResources().getString(R.string.english_level);
+    String geography_level = context.getResources().getString(R.string.geography_level);
+
+
+    @Test
+    public void whenMathButtonIsClicked(){
+        //Given
+        onView(withId(R.id.math_Button)).check(matches(withText(maths_button)));
+        onView(withId(R.id.math_Button)).check(matches(withText(maths_button)));
+        onView(withId(R.id.math_Button)).check(matches(withText(maths_button)));
+        onView(withId(R.id.math_level_id)).check(matches(withText(math_level)));
+
+        //Then
+        onView(withId(R.id.math_Button)).perform(click());
+    }
 }
