9b4da9cab59ced66205116d554905d7fac29fbff
Thunderscythe <cyserser@gmail.com>
2020-04-10 21:09:50 +0100

    Commit de prueba

--- app/src/main/java/es/ulpgc/eite/da/learnquest/question/QuestionActivity.java
+++ app/src/main/java/es/ulpgc/eite/da/learnquest/question/QuestionActivity.java
@@ -11,7 +11,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import es.ulpgc.eite.da.learnquest.R;
-
+//hola
 public class QuestionActivity
         extends AppCompatActivity implements QuestionContract.View {
 
