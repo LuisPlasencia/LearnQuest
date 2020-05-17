58b84cbbda70d529dfc6afe8d5e0ece919d4256a
Thunderscythe <cyserser@gmail.com>
2020-05-17 18:41:48 +0100

    T3.1 Probando algunos tests en la pantalla Login

--- app/src/androidTest/java/es/ulpgc/eite/da/learnquest/intrumentedTests/InstrumentedTests.java
+++ app/src/androidTest/java/es/ulpgc/eite/da/learnquest/intrumentedTests/InstrumentedTests.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.view.ViewParent;
 
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
@@ -24,8 +25,10 @@ import es.ulpgc.eite.da.learnquest.R;
 import es.ulpgc.eite.da.learnquest.finalQuiz.FinalQuizActivity;
 import es.ulpgc.eite.da.learnquest.login.LoginActivity;
 
+import static android.app.PendingIntent.getActivity;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
@@ -35,6 +38,13 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
+
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -47,11 +57,78 @@ public class InstrumentedTests {
             InstrumentationRegistry.getInstrumentation().getTargetContext();
 
     @Test
-    public void test1() {
+    public void letsGoButton() {
         //GIVEN
         onView(withId(R.id.login_title)).check(matches(withText("LearnQuest")));
+        onView(withId(R.id.login_title2)).check(matches(withText("Have fun learning!")));
+        ViewInteraction editText = onView(withId(R.id.username_input));
+        editText.check(matches(withText("")));
+        ViewInteraction editText2 = onView(withId(R.id.password_input));
+        editText2.check(matches(withText("")));
+
         //WHEN
+        onView(withId(R.id.username_input)).perform(typeText("c"));
+        onView(withId(R.id.password_input)).perform(typeText("c"));
+        pressBack();
+        ViewInteraction appCompatButton = onView(withId(R.id.lets_go_button));
+        appCompatButton.perform(click());
         //THEN
+        ViewInteraction button = onView((withId(R.id.go_button)));
+        button.check(matches(isDisplayed()));
     }
-    
+
+    @Test
+    public void letsGoButtonWrongUserName() {
+        //GIVEN
+        onView(withId(R.id.login_title)).check(matches(withText("LearnQuest")));
+
+        onView(withId(R.id.login_title2)).check(matches(withText("Have fun learning!")));
+
+        ViewInteraction editText = onView(withId(R.id.username_input));
+        editText.check(matches(withText("")));
+
+        ViewInteraction editText2 = onView(withId(R.id.password_input));
+        editText2.check(matches(withText("")));
+
+        //WHEN
+        onView(withId(R.id.username_input)).perform(typeText("cc"));
+
+        onView(withId(R.id.password_input)).perform(typeText("c"));
+
+        pressBack();
+
+        ViewInteraction appCompatButton = onView(withId(R.id.lets_go_button));
+        appCompatButton.perform(click());
+
+        //THEN
+        onView(withText("Invalid username")).inRoot(withDecorView(not(mActivityTestRule.getActivity().getWindow().getDecorView()))).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void letsGoButtonWrongPassWord() {
+        //GIVEN
+        onView(withId(R.id.login_title)).check(matches(withText("LearnQuest")));
+
+        onView(withId(R.id.login_title2)).check(matches(withText("Have fun learning!")));
+
+        ViewInteraction editText = onView(withId(R.id.username_input));
+        editText.check(matches(withText("")));
+
+        ViewInteraction editText2 = onView(withId(R.id.password_input));
+        editText2.check(matches(withText("")));
+
+        //WHEN
+        onView(withId(R.id.username_input)).perform(typeText("c"));
+
+        onView(withId(R.id.password_input)).perform(typeText("cc"));
+
+        pressBack();
+
+        ViewInteraction appCompatButton = onView(withId(R.id.lets_go_button));
+        appCompatButton.perform(click());
+
+        //THEN
+        onView(withText("Wrong password")).inRoot(withDecorView(not(mActivityTestRule.getActivity().getWindow().getDecorView()))).check(matches(isDisplayed()));
+    }
+
 }
